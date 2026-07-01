43ff5616436a405f751edfd9b89fddfa203da29e
AlisaNyaXD <55809431+AlisaNya@users.noreply.github.com>
2026-07-01 23:34:31 +0500

    NewUpdate
    добавлена возможность установки или изменения направления печати
    
    добавлено автоматическое выравнивание текста с учетом направления печати (опционально)
    
    добавлен режим линейного вывода (по порядку а не только рандом)
    
    в линейном режиме поддерживается событие через делегат по окончании печати набора символов а так-же опционально автоматическая остановка печати
    
    функции начала печати теперь имеет опции рандомизировать-ли ее и предварительная очистка
    
    в функции остановки печати то-же можно указать только остановить или еще и очистить
    
    если активно автоматическое выравнивание то меняя направление печати через переменную оно применится только после вызова функции начала печати если-же менять через функции смены направления оно применится сразу не требуя остановок печати да-же если печать уже идет
    
    добавлен BP_Demo
    добавлен виджет WBP_TypeWriter (пример использования)

--- Source/UnrealEssenceLite/Private/UI/TypingEffectWidget.cpp
+++ Source/UnrealEssenceLite/Private/UI/TypingEffectWidget.cpp
@@ -12,36 +12,88 @@ void UTypingEffectWidget::NativeConstruct()
 	}
 }
 
-void UTypingEffectWidget::StartTypingEffect()
+void UTypingEffectWidget::StartTypingEffect(bool bRandomize, bool bStartFromClear)
 {
-	CurrentLine.Empty();
+	bRandomIsActive = bRandomize;
+	if (bStartFromClear)	{ CurrentLine.Empty();	};
+	if (bJustifyIsAuto)		{ UpdateJustify();		};
 	UpdateDisplay();
 	GetWorld()->GetTimerManager().SetTimer(
 		TypingTimerHandle,
 		this,
-		&UTypingEffectWidget::AddRandomCharacter,
+		&UTypingEffectWidget::AddCharacter,
 		TypingInterval,
 		true
 	);
 }
 
-void UTypingEffectWidget::StopTypingEffect()
+void UTypingEffectWidget::StopTypingEffect(bool bClear)
 {
 	GetWorld()->GetTimerManager().ClearTimer(TypingTimerHandle);
+	
+	if (bClear)
+	{
+		CurrentLine.Empty();
+		UpdateDisplay();
+	}
 }
 
-void UTypingEffectWidget::AddRandomCharacter()
+void UTypingEffectWidget::ChangeDirection()
+{
+	if (CurrentDirection == ETypingDirection::LeftToRight)
+	{
+		CurrentDirection = ETypingDirection::RightToLeft;
+	}
+	else
+	{
+		CurrentDirection = ETypingDirection::LeftToRight;
+	}
+	if (bJustifyIsAuto) { UpdateJustify(); }
+	
+}
+
+void UTypingEffectWidget::SetDirection(ETypingDirection NewDirection)
+{
+	CurrentDirection = NewDirection;
+	if (bJustifyIsAuto) { UpdateJustify(); }
+}
+
+void UTypingEffectWidget::AddCharacter()
 {
 	if (AllowedChars.IsEmpty()) return;
-
-	int32 Index = FMath::RandRange(0, AllowedChars.Len() - 1);
-	TCHAR NewChar = AllowedChars[Index];
-	CurrentLine.AppendChar(NewChar);
-
-	// Если строка слишком длинная — удаляем первый символ (эффект "бегущей строки")
-	if (CurrentLine.Len() > MaxLineLength)
+	
+	if (bRandomIsActive)
 	{
-		CurrentLine.RemoveAt(0, 1);
+		CharIndex = FMath::RandRange(0, AllowedChars.Len() - 1);
+	}
+	else
+	{
+		CharIndex += 1;
+		if (CharIndex > AllowedChars.Len() - 1)
+		{
+			CharIndex = 0;
+			if (OnAllAllowedCharsIsEndTyping.IsBound())
+			{
+				OnAllAllowedCharsIsEndTyping.Broadcast();
+			}
+			
+			if (bUseAutoStopTyping)
+			{
+				StopTypingEffect(false);
+			}
+		};
+	}
+	
+	TCHAR NewChar = AllowedChars[CharIndex];
+
+	switch (CurrentDirection)
+	{
+	case ETypingDirection::LeftToRight:
+		AddCharacterLeftToRight(NewChar);
+		break;
+	case ETypingDirection::RightToLeft:
+		AddCharacterRightToLeft(NewChar);
+		break;
 	}
 
 	UpdateDisplay();
@@ -54,3 +106,42 @@ void UTypingEffectWidget::UpdateDisplay()
 		DisplayText->SetText(FText::FromString(CurrentLine));
 	}
 }
+
+void UTypingEffectWidget::AddCharacterLeftToRight(TCHAR NewChar)
+{
+	// Добавляем символ в конец справа
+	CurrentLine.AppendChar(NewChar);
+
+	// Если строка слишком длинная - удаляем первый символ (слева)
+	if (CurrentLine.Len() > MaxLineLength)
+	{
+		CurrentLine.RemoveAt(0, 1);
+	}
+}
+
+void UTypingEffectWidget::AddCharacterRightToLeft(TCHAR NewChar)
+{
+	// Вставляем символ в начало строки (индекс 0) слева
+	CurrentLine.InsertAt(0, NewChar);
+
+	// Если строка слишком длинная - удаляем последний символ (справа)
+	if (CurrentLine.Len() > MaxLineLength)
+	{
+		CurrentLine.RemoveAt(CurrentLine.Len() - 1, 1);
+	}
+}
+
+void UTypingEffectWidget::UpdateJustify()
+{
+	if (DisplayText)
+	{
+		if (CurrentDirection == ETypingDirection::LeftToRight)
+		{
+			DisplayText->SetJustification(ETextJustify::Type::Left);
+		}
+		else
+		{
+			DisplayText->SetJustification(ETextJustify::Type::Right);
+		}
+	}
+};
--- Source/UnrealEssenceLite/Public/UI/TypingEffectWidget.h
+++ Source/UnrealEssenceLite/Public/UI/TypingEffectWidget.h
@@ -8,23 +8,49 @@
 #include "TimerManager.h"
 #include "TypingEffectWidget.generated.h"
 
+UENUM(BlueprintType)
+enum class ETypingDirection : uint8
+{
+	LeftToRight UMETA(DisplayName = "Слева -> направо"),
+	RightToLeft UMETA(DisplayName = "Справа <- налево")
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAllAllowedCharsIsEndTypingEvent); // в упорядоченном режиме уведомляет о завершении печати переданного набора символов / строки
+
+/**
+ * @example
+ * - Создайте виджет-блюпринт на основе UTypingEffectWidget.
+ * - Добавьте в него TextBlock и назовите DisplayText (строго как в коде).
+ * - В Event Construct или по кнопке вызовите Start Typing Effect.
+ */
 UCLASS()
 class UNREALESSENCELITE_API UTypingEffectWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 public:
+	
+	// События
+	UPROPERTY(BlueprintAssignable, Category = "TypingEffect")
+	FOnAllAllowedCharsIsEndTypingEvent OnAllAllowedCharsIsEndTyping;
+	
 	UFUNCTION(BlueprintCallable, Category = "TypingEffect")
-	void StartTypingEffect();
+	void StartTypingEffect(bool bRandomize = true, bool bStartFromClear = false);
 
 	UFUNCTION(BlueprintCallable, Category = "TypingEffect")
-	void StopTypingEffect();
+	void StopTypingEffect(bool bClear = false);
 
+	UFUNCTION(BlueprintCallable, Category = "TypingEffect")
+	void ChangeDirection();
+
+	UFUNCTION(BlueprintCallable, Category = "TypingEffect")
+	void SetDirection(ETypingDirection NewDirection);
+	
 protected:
 	virtual void NativeConstruct() override;
 
 	UPROPERTY(meta = (BindWidget))
-	UTextBlock* DisplayText; // Привязать в BP
+	UTextBlock* DisplayText; // Привязать в UI
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
 	float TypingInterval = 0.05f; // Скорость появления символов
@@ -33,12 +59,29 @@ protected:
 	int32 MaxLineLength = 40; // Длина строки до сброса
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
-	FString AllowedChars = TEXT("ABCD1234!@#$"); // Можно "01" или "ABCD1234!@#$"
+	FString AllowedChars = TEXT("ABCD1234!@#$"); // Набор символов для печати "ABCD1234!@#$" (читается по порядку либо рандомно)
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	ETypingDirection CurrentDirection = ETypingDirection::LeftToRight;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	bool bRandomIsActive = false; // рандомизация
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	bool bJustifyIsAuto = false; // автоматизация управления выравниванием текста
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	bool bUseAutoStopTyping = false; // автоматическая остановка печати по окончании вывода набора символов
+	
 private:
 	FString CurrentLine;
 	FTimerHandle TypingTimerHandle;
-
-	void AddRandomCharacter();
+	int32 CharIndex = 0;
+	
+	void AddCharacter();
 	void UpdateDisplay();
+	
+	void AddCharacterLeftToRight(TCHAR NewChar);
+	void AddCharacterRightToLeft(TCHAR NewChar);
+	void UpdateJustify();
 };
